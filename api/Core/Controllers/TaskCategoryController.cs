e5026ff2cfa66183758f23ed4ef02ebae5c57ade
Patryk Rubaj <patryk.rubaj@gmail.com>
2025-03-06 19:26:21 +0100

    Added authorization to TaskCategoryController.cs endpoints

--- api/Core/Controllers/TaskCategoryController.cs
+++ api/Core/Controllers/TaskCategoryController.cs
@@ -1,46 +1,56 @@
-﻿using Core.DataAccess;
+﻿using System.Data;
+using Core.DataAccess;
 using Core.Model;
 using Core.Model.DTO;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Core.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("[controller]")]
 public class TaskCategoryController: ControllerBase
 {
     private readonly DataContext _dataContext;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public TaskCategoryController(DataContext dataContext)
+    public TaskCategoryController(DataContext dataContext, UserManager<IdentityUser> userManager)
     {
         _dataContext = dataContext;
+        _userManager = userManager;
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<TaskCategory>> GetById(int id)
     {
-        var result = await _dataContext.TaskCategories.FirstOrDefaultAsync(tc => tc.Id == id);
+        var userId = _userManager.GetUserId(User) ?? throw new NoNullAllowedException();
+        
+        var result = await _dataContext.TaskCategories.FirstOrDefaultAsync(tc => tc.Id == id && tc.OwnerId == userId);
         return result == null ? NotFound() : Ok(result);
     }
 
     [HttpGet]
     public async Task<ActionResult<IList<TaskCategory>>> GetAll()
     {
-        return await _dataContext.TaskCategories.ToListAsync();
+        var userId = _userManager.GetUserId(User) ?? throw new NoNullAllowedException();
+        
+        return await _dataContext.TaskCategories.Where(tc => tc.OwnerId == userId).ToListAsync();
     }
 
     [HttpPost]
-    [Authorize]
     public async Task<ActionResult> Add([FromBody] TaskCategoryAddDto taskCategory)
     {
+        var userId = _userManager.GetUserId(User) ?? throw new NoNullAllowedException();
+        
         var newTaskCategory = new TaskCategory()
         {
             Name = taskCategory.CategoryName,
             StartTime = taskCategory.StartTime,
             EndTime = taskCategory.EndTime,
-            OwnerId = 1
+            OwnerId = userId
         };
         
         await _dataContext.TaskCategories.AddAsync(newTaskCategory);
